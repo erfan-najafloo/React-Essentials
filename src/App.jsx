7461e3b7aa4873c1ddc7790f12ab7cb2a5d03528
Erfan Najafloo <erfan.najafloo@gmail.com>
2025-10-24 11:11:29 +0330

    feat: initialize project structure
    Adds the base project setup and initial component structure. Components utilize props to render dynamic content, laying the foundation for interactive features.

---
+++ src/App.jsx
@@ -0,0 +1,56 @@
+import reactImage from "./assets/react-core-concepts.png";
+import componentImg from "./assets/components.png";
+const reactDescriptions = ["Fundamental", "Crucial", "Core"];
+
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
+function Header() {
+  const description = reactDescriptions[genRandomInt(2)];
+  return (
+    <header>
+      <img src={reactImage} alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        {description} React concepts you will need for almost any app you are
+        going to build!
+      </p>
+    </header>
+  );
+}
+
+function CoreConsept(props) {
+  return (
+    <li>
+      <img src={props.image} alt={props.title} />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <section id="core-concepts">
+          <h2>Core Consepts</h2>
+          <ul>
+            <CoreConsept
+              image={componentImg}
+              title="Components"
+              description="The core UI bulding block"
+            />
+            <CoreConsept />
+            <CoreConsept />
+            <CoreConsept />
+          </ul>
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
