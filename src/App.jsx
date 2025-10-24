c217531027227d9e1374defc493e1d57db3f5ab0
Erfan Najafloo <erfan.najafloo@gmail.com>
2025-10-24 13:04:49 +0330

    refactor: extract Header and CoreConcept components into separate files

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,6 @@
-import reactImage from "./assets/react-core-concepts.png";
-import componentImg from "./assets/components.png";
 import { CORE_CONCEPTS } from "./data";
-const reactDescriptions = ["Fundamental", "Crucial", "Core"];
-
-function genRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-function Header() {
-  const description = reactDescriptions[genRandomInt(2)];
-  return (
-    <header>
-      <img src={reactImage} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {description} React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  );
-}
-
-function CoreConsept({ image, title, description }) {
-  return (
-    <li>
-      <img src={image} alt={title} />
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </li>
-  );
-}
+import Header from "./components/Header";
+import CoreConsept from "./components/CoreConsept";
 
 function App() {
   return (
